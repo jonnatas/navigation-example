10e68cafb30d13a87e4748e6b7ea6ff96a7fceb5
jonnatasLennon <jonnatas.lennon@aisdigital.com.br>
2019-10-11 09:41:37 -0300

    first commit

--- app/src/main/java/com/example/forumnavigator/ThreeFragment.kt
+++ app/src/main/java/com/example/forumnavigator/ThreeFragment.kt
@@ -2,31 +2,30 @@ package com.example.forumnavigator
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
-import kotlinx.android.synthetic.main.fragment_two.*
+import kotlinx.android.synthetic.main.fragment_three.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class TwoFragment : Fragment() {
+class ThreeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two, container, false)
+        return inflater.inflate(R.layout.fragment_three, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        buttonFour.setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_twoFragment_to_fourFragment)
+        buttonToNavSend.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_threeFragment_to_nav_send2)
         )
     }
-
 }
--- app/src/main/java/com/example/forumnavigator/ui/home/OneFragment.kt
+++ app/src/main/java/com/example/forumnavigator/ui/home/OneFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.forumnavigator.R
+import kotlinx.android.synthetic.main.one_fragment.*
 
 
 class OneFragment : Fragment() {
@@ -21,4 +23,10 @@ class OneFragment : Fragment() {
         return inflater.inflate(R.layout.one_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonToThree.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_oneFragment_to_threeFragment)
+        )
+    }
 }
